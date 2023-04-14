d0e4bddbb823d358aeffc9637360460e02c0b76a
OkolieOKS234 <dokolie040@gmail.com>
2023-04-14 13:22:40 +0100

    added a little responsiveness

---
+++ script.js
@@ -0,0 +1,31 @@
+(function() {
+    var rotate, timeline;
+  
+    rotate = function() {
+      return $('.card1:first-child').fadeOut(400, 'swing', function() {
+        return $('.card1:first-child').appendTo('.container').hide();
+      }).fadeIn(400, 'swing');
+    };
+  
+    timeline = setInterval(rotate, 1000);
+  
+    $('body').hover(function() {
+      return clearInterval(timeline);
+    });
+  
+  }).call(this);
+  
+
+  
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-36251023-1']);
+    _gaq.push(['_setDomainName', 'jqueryscript.net']);
+    _gaq.push(['_trackPageview']);
+  
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+
+    
